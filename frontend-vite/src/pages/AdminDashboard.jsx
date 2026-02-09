9c415696f0f2e5cd1177316430471f28bdb552cd
mctajo1-bot77 <mctajo1@gmail.com>
2026-02-09 00:29:08 -0500

    Update AdminDashboard.jsx

--- frontend-vite/src/pages/AdminDashboard.jsx
+++ frontend-vite/src/pages/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import apiClient from '@/services/apiClient';
+import apiClient from '@/api'; // Nuevo path
 import { useNavigate } from 'react-router-dom';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
