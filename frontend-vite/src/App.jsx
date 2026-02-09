7438c264a6e8db7f20cc2a86a2cabef0055d6579
mctajo1-bot77 <mctajo1@gmail.com>
2026-02-09 00:40:59 -0500

    Update App.jsx

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import apiClient from '@/api'; // Nuevo path
 import { useNavigate } from "react-router-dom";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
